625fa91472f5682ebc96e232cd3307c6633f4b82
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-01-27 15:59:43 +0100

    Optionally use supplied cuts for theta plot

--- cta_tools/scripts/plot_theta2.py
+++ cta_tools/scripts/plot_theta2.py
@@ -1,4 +1,5 @@
 from astropy.table import vstack
+from aict_tools.io import read_data
 import matplotlib.pyplot as plt
 import re
 from pathlib import Path
@@ -15,32 +16,44 @@ from cta_tools.io import read_table
 @click.option('--offs', '-w', multiple=True, default=[1])
 @click.option('--runs', type=str, default='*')
 @click.option('--min_energy', type=int, default=1)
-def main(input_folder, output, offs, runs, min_energy):
+def main(input_folder, output, offs, runs, min_energy, theta_cut=False):
     reg = re.compile(runs)
     input_files = Path(input_folder).glob("**/*")
     run_tables = []
     live_time = 0
     runs = []
     for f in input_files:
+        
         if reg.match(str(f)):
+            print(f)
             t = read_table(f)
-            emask = t['ENERGY'] > min_energy*u.GeV
-            run_tables.append(t[emask])
-            live_time += t.meta["LIVETIME"]
-            runs.append(f.stem.split('.')[0])
+            cuts = read_data(f, 'cuts')
+            emask = t['gamma_energy_prediction'] > min_energy/100#*u.GeV
+            gh_mask = cuts['passed_gh']
+            theta_mask = cuts['passed_theta']
+            selected = emask & gh_mask #& theta_mask
+            if theta_cut:
+                selected &= theta_mask
+            run_tables.append(t[selected.values])
+
+            number = re.search(r'([0-9]{5})', f.stem).group(0)
+            live_time += 0#t.meta["LIVETIME"]
+            runs.append(int(number))
 
     runs.sort()
     events = vstack(run_tables)
-    theta_cuts = events['THETA_CUT_VALUE']
-    theta_on = events['THETA_ON'][events['THETA_ON'] < theta_cuts]
+    #theta_cuts = events['THETA_CUT_VALUE']
+    theta_on = events['theta_on']#[events['THETA_ON'] < theta_cuts]
+    print('On events:', len(theta_on))
     off_thetas = []
     alpha = 1/len(offs)
     for i in offs:
-        off = events[f"THETA_OFF{i}"][events[f"THETA_OFF{i}"] < theta_cuts]
+        off = events[f"theta_off{i}"]#[events[f"THETA_OFF{i}"] < theta_cuts]
         print(len(off))
         off_thetas.append(off.data)
 
     theta_off = np.concatenate(off_thetas)
+
 
     plt.figure()
     ax = plt.gca()
@@ -49,7 +62,7 @@ def main(input_folder, output, offs, runs, min_energy):
         theta_on**2,
         theta_off**2,
         scaling=alpha,
-        cut=10,  # dummy
+        cut=0.5,  # dummy
         ontime=live_time*u.s,
         ax=ax,
         window=[0, xmax],
