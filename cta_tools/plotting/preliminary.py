41f867dd5ed74425555d49ecb5769f86c4f9ff4f
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-01-20 16:29:17 +0100

    Try to improve the representation
    - Ideas taken from https://github.com/rougier/scientific-visualization-book
    - Untested, probably needs tweaking

--- cta_tools/plotting/preliminary.py
+++ cta_tools/plotting/preliminary.py
@@ -1,5 +1,6 @@
 import matplotlib.axes
 from matplotlib.offsetbox import AnchoredText
+from matplotlib.patheffects import Stroke, Normal
 
 class PreliminaryAxes(matplotlib.axes.Axes):
     def __init__(self, *args, **kwargs):
@@ -7,9 +8,25 @@ class PreliminaryAxes(matplotlib.axes.Axes):
         super().__init__(*args, **kwargs)
         # if "main" axes 
         if not kwargs.get("sharex"):
-            text = AnchoredText("PRELIMINARY", loc="center", frameon=False,
-                              borderpad=0, prop=dict(alpha=0.15, fontsize=50, rotation=30))
-            text.set_zorder(4)
+            text = AnchoredText(
+                "PRELIMINARY",
+                loc="center",
+                frameon=False,
+                borderpad=0,
+                prop=dict(
+                    alpha=0.15,
+                    fontsize=50,
+                    rotation=30,
+                    color="black"
+                )
+            )
+            text.set_path_effects(
+                [
+                    Stroke(linewidth=3, foreground="white"),
+                    Normal()
+                ]
+            )
+            text.set_zorder(1000)
             self.add_artist(text)
 
 matplotlib.axes.Axes = PreliminaryAxes
